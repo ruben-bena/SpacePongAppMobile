380360178725f914a58fa2d0d22c34a4e061f3ab
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-11-10 19:28:49 +0100

    WIP view functionality

--- app/src/main/java/com/matrixplay6/spacepongappmobile/RegisterActivity.kt
+++ app/src/main/java/com/matrixplay6/spacepongappmobile/RegisterActivity.kt
@@ -5,8 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+import androidx.core.widget.doOnTextChanged
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var nameInput: TextInputEditText
+    private lateinit var uriInput: TextInputEditText
+    private lateinit var registerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,23 @@ class RegisterActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        nameInput = findViewById(R.id.nameInput)
+        uriInput = findViewById(R.id.uriInput)
+        registerButton = findViewById(R.id.registerButton)
+
+        // Input listeners
+        nameInput.doOnTextChanged { text, _, _, _ ->
+            decideButtonVisibility()
+        }
+        uriInput.doOnTextChanged { text, _, _, _ ->
+            decideButtonVisibility()
+        }
+    }
+
+    private fun decideButtonVisibility() {
+        val isNameFilled = nameInput.text?.isNotBlank() == true
+        val isUriFilled = uriInput.text?.isNotBlank() == true
+        registerButton.isEnabled = isNameFilled && isUriFilled
     }
 }
