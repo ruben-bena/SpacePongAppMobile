0db62e87694f4480e125a254ba4df92aa471222f
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-11-18 19:02:04 +0100

    View CountdownActivity created

---
+++ app/src/main/java/com/matrixplay6/spacepongappmobile/CountdownActivity.kt
@@ -0,0 +1,20 @@
+package com.matrixplay6.spacepongappmobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class CountdownActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_countdown)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
