17d447937dfb5f97976cf27fad6f77337a338974
ruben-bena <181500053+ruben-bena@users.noreply.github.com>
2025-11-18 18:47:52 +0100

    WaitActivity is prettier now

--- app/src/main/java/com/matrixplay6/spacepongappmobile/WaitActivity.kt
+++ app/src/main/java/com/matrixplay6/spacepongappmobile/WaitActivity.kt
@@ -1,20 +1,12 @@
 package com.matrixplay6.spacepongappmobile
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class WaitActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_wait)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
